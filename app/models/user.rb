70c722cbd515b7d598e581eb4aaa06543634f3a8
Yuya-Toudou <yu.toudou@gmail.com>
2020-11-09 18:43:57 +0900

    エラーメッセージの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email, uniqueness: true
-  validates :name, presence: true
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)/i }
-
-  validates :last_name_full, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ } 
-  validates :first_name_full, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ } 
-  validates :last_name_kana, presence: true,  format: { with: /\A[ァ-ン]/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]/ }
-  validates :birth_date, presence: true
+         validates :email, uniqueness: true
+         validates :name, presence: true
+         validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)/i }
+       
+         validates :last_name_full, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ } 
+         validates :first_name_full, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ } 
+         validates :last_name_kana, presence: true,  format: { with: /\A[ァ-ン]/ }
+         validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]/ }
+         validates :birth_date, presence: true
 end
