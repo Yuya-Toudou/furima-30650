c8690ebea2147b19dfa7bbbf329bdd3f13b43965
Yuya-Toudou <yu.toudou@gmail.com>
2020-11-10 15:39:40 +0900

    バリデーション設定の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,22 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :email, uniqueness: true
-         validates :name, presence: true
          validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)/i }
-       
-         validates :last_name_full, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ } 
-         validates :first_name_full, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ } 
-         validates :last_name_kana, presence: true,  format: { with: /\A[ァ-ン]/ }
-         validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]/ }
+
+         with_options presence: true do
+          validates :name
+
+           with_options format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
+            validates :last_name_full
+            validates :first_name_full
+          end
+
+          with_options format: { with: /\A[ァ-ン]/ } do
+            validates :last_name_kana
+            validates :first_name_kana
+          end
+          
          validates :birth_date, presence: true
+        end
+
 end
