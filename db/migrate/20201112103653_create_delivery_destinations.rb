aed1dcae0a78c069f1a2a32d56f1439d33641620
Yuya-Toudou <yu.toudou@gmail.com>
2020-11-12 19:44:40 +0900

    マイグレーションの実行

--- db/migrate/20201112103653_create_delivery_destinations.rb
+++ db/migrate/20201112103653_create_delivery_destinations.rb
@@ -1,7 +1,13 @@
 class CreateDeliveryDestinations < ActiveRecord::Migration[6.0]
   def change
     create_table :delivery_destinations do |t|
-
+      t.references :buy,            null: false,  foreign_key: true
+      t.string     :zip_code,       null: false
+      t.integer    :prefectures_id, null: false
+      t.string     :municipality,   null: false
+      t.string     :address,        null: false
+      t.string     :house_name
+      t.string     :phone_number,   null: false
       t.timestamps
     end
   end
