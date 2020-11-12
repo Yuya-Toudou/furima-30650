9d81f3cee753955e512a0cfdd95f52e8375f5949
Yuya-Toudou <yu.toudou@gmail.com>
2020-11-12 14:21:27 +0900

    コントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,19 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    redirect_to root_path if user_signed_in? && current_user.id != @item.user_id
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
+
+
   private
 
   def item_params
