d55549f92903e01f8376aa5509bd8ea52298cd42
Yuya-Toudou <yu.toudou@gmail.com>
2020-11-12 14:41:05 +0900

    コントローラーの再修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
   def index
     @items = Item.all.order(created_at: 'DESC')
   end
@@ -21,9 +22,6 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  def edit
-    redirect_to root_path if user_signed_in? && current_user.id != @item.user_id
-  end
 
   def update
     if @item.update(item_params)
@@ -37,5 +35,9 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :postage_pay_id, :prefectures_id, :posting_date_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
