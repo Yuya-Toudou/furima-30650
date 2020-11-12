c53864069f701fdc265116828761268ded81402e
Yuya-Toudou <yu.toudou@gmail.com>
2020-11-12 23:09:02 +0900

    アクションの再定義

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -2,14 +2,21 @@ class BuysController < ApplicationController
   before_action :set_item
 
   def index
-    @buy = Buy.new
+    @buy = BuyAddress.new
   end
 
   def create
+    @buy = BuyAddress.new(buy_params)
+    @buy.save
+    redirect_to root_path
   end
 
 
   private
+
+  def buy_params
+    params.require(:delivery_address).permit(:zip_code, :prefectures_id, :municipality, :address, :house_name, :phone_number).merge(item_id: params[:item_id], token: params[:token])
+  end
   
   def set_item
     @item = Item.find(params[:item_id])
