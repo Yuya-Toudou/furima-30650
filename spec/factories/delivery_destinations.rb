fca5e0e8ab788be2b576652bbc78ebdae5992476
Yuya-Toudou <yu.toudou@gmail.com>
2020-11-13 12:28:50 +0900

    テストコードの実装

--- spec/factories/delivery_destinations.rb
+++ spec/factories/delivery_destinations.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
-  factory :delivery_destination do
-    
+  factory :buy_address do
+    token { 'tok_1234567890abcdefghijklmnopqr' }
+    zip_code { '123-4567' }
+    prefectures_id { 1 }
+    municipality { 'テスト村' }
+    address { '1-1' }
+    house_name { 'テストハイツ' }
+    phone_number { '09012345678' }
   end
 end
